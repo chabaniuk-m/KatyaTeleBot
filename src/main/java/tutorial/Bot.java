d6d921bf2c6ef2b1c1c63a5001b1399346bad0cc
chabaniuk-m <chabanyuk.maks@gmail.com>
2024-03-04 12:03:20 +0200

    send messages

--- src/main/java/tutorial/Bot.java
+++ src/main/java/tutorial/Bot.java
@@ -1,12 +1,16 @@
 package tutorial;
 
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class Bot extends TelegramLongPollingBot {
     Integer num = null;
@@ -14,9 +18,46 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         Message msg = update.getMessage();
-        User user = msg.getFrom();
+        if (msg.isCommand()) {
+            if (msg.getText().equals("/time")) {
+                executeTimeCommand(msg);
+            }
+        }
+    }
 
-        System.out.printf("Користувач %s написав %s\n", user.getFirstName() + user.getLastName(), msg.getText());
+    private void executeTimeCommand(Message msg) {
+        LocalDateTime time = LocalDateTime.now();
+        String[] strTime = time.toString().split("T");
+        String[] str = strTime[1].split("\\.");
+        String text = str[0];
+        sendText(msg.getChatId(), text);
+    }
+
+    private void addTwoNumber(Message msg) {
+        if (num == null) {
+            num = Integer.valueOf(msg.getText());
+        } else {
+            calcSum(msg.getFrom().getId(), msg.getText());
+        }
+    }
+
+    private void calcSum(Long chatID, String secondValue) {
+        num2 = Integer.valueOf(secondValue);
+        int sum = num + num2;
+        String text = "Sum of two numbers: " + sum;
+        sendText(chatID, text);
+        num = null;
+        num2 = null;
+    }
+
+
+    public void sendText(Long chatID, String text) {
+        SendMessage sm = SendMessage.builder().chatId(chatID).text(text).build();
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Bot(String botToken) {
